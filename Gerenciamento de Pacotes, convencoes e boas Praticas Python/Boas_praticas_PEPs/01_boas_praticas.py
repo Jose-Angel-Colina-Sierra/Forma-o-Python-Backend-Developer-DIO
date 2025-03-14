ea18de1c072cf205a841b03459302f960a839dbf
Jose Colina <ellocojoseangelcolinasierra07@gmail.com>
2025-03-14 01:03:27 -0300

    Adicionando todos os arquivos de gerenciamento de pacotes na pasta de gerenciamento de pacotes

---
+++ Gerenciamento de Pacotes, convencoes e boas Praticas Python/Boas_praticas_PEPs/01_boas_praticas.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+frutas = ["pera", "maca"]
+
+
+frutas = [
+    "pera",
+    "maca",
+    "pera",
+    "calor",
+    "maca",
+    "pera",
+    "calor",
+    "maca",
+    "pera",
+    "calor",
+    "maca",
+    "pera",
+    "calor",
+]
+
+
+class Cachorro:
+    pass
+
+
+print("")
+
+
+carros = ["ferrari", "gol", "up", "brasilia"]
---
+++ Gerenciamento de Pacotes, convencoes e boas Praticas Python/Boas_praticas_PEPs/pep-8.py
@@ -0,0 +1,23 @@
+# https://peps.python.org/pep-0008/
+
+# Nomes variaveis:
+
+# funcoes e variaveis:
+
+snake_case = "snake_case"
+
+
+def somar(arguemnto1, argumento2):
+    pass
+
+
+# Classes:
+
+CamelCase = "CamelCase"
+
+
+class ContaBancaria:
+    pass
+
+
+print("aqui")
